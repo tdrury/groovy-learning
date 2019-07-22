0a25911de5b51a0050417ba9068b884ac7bfb072
Tim Drury <t.drury@sap.com>
2019-07-22 15:26:53 -0400

    more cli tests

--- src/test/groovy/cli/CliSpec.groovy
+++ src/test/groovy/cli/CliSpec.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 
 class CliSpec extends Specification {
 
-	def "simple help"() {
+	def "boolean arg"() {
 
 		given:
 		def args = "-h".split()
@@ -22,22 +22,83 @@ class CliSpec extends Specification {
 		options.h
 	}
 
-	def "get a string argument"() {
+	def "string arg"() {
 
 		given:
-		def args = "-h -f /foo/bar.txt".split()
 		def cli = new CliBuilder(usage: "groovy ${this.class.simpleName} [<options>]", header: "options:")
 		cli.with {
 			h longOpt: 'help', "Show usage information"
 			f (longOpt: 'file', args: 1, argName: 'file', "a file ...")
-
 		}
 
 		when:
-		def options = cli.parse(args)
+		def options = cli.parse("-h -f /foo/bar.txt".split())
 
 		then:
 		options.f == "/foo/bar.txt"
 	}
+
+	def "two args"() {
+
+		given:
+		def cli = new CliBuilder()
+		cli.with {
+			p (longOpt: 'params', args: 2, argName: 'params', "a couple parameters")
+		}
+
+		when:
+		def options = cli.parse("-p one two".split())
+
+		then:
+		options.ps == ["one", "two"] // note plural 'p'
+	}
+
+	def "two args with comma separator"() {
+
+		given:
+		def cli = new CliBuilder()
+		cli.with {
+			p (longOpt: 'params', args: 2, valueSeparator: ',', argName: 'params', "a couple parameters")
+		}
+
+		when:
+		def options = cli.parse("-p one,two".split())
+
+		then:
+		options.ps == ["one", "two"] // note plural 'p'
+	}
+
+	def "Integer arg"() {
+
+		given:
+		def cli = new CliBuilder()
+		cli.with {
+			i (longOpt: 'int', args: 1, argName: 'int', type: Integer, "a integer")
+		}
+
+		when:
+		def options = cli.parse("-i 1234".split())
+
+		then:
+		options.i == 1234
+	}
+
+	def "default value arg"() {
+
+		given:
+		def cli = new CliBuilder()
+		cli.with {
+			i (longOpt: 'int', args: 1, argName: 'int', type: Integer, defaultValue: "999", "a integer")
+		}
+
+		when:
+		def options = cli.parse("-i 1234".split())
+		def optionsDefaultArgs = cli.parse("".split())
+
+		then:
+		options.i == 1234
+		optionsDefaultArgs.i == 999
+	}
+
 
 }
